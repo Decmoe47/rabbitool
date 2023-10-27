6da2f3b72e6b56ece7e9ccd31d5c29bcb892ca5a
Decmoe47 <lingsq47@outlook.com>
2023-10-27 11:03:44 +0800

    fix: url转换遗留

--- Rabbitool/Extension.cs
+++ Rabbitool/Extension.cs
@@ -7,22 +7,13 @@ public static partial class Extension
 {
     public static string AddRedirectToUrls(this string text)
     {
-        if (text == "")
-        {
-            text = "（无文本）";
-        }
-        else
-        {
-            text = UrlRegex().Replace(text, m => Configs.R.RedirectUrl + m.Value);
-            text = BLiveRegex().Replace(text, m => Configs.R.RedirectUrl + "https://" + m.Value);
-        }
+        text = text == "" 
+            ? "（无文本）" 
+            : UrlRegex().Replace(text, m => Configs.R.RedirectUrl + m.Value);
 
         return text;
     }
 
-    [GeneratedRegex(@"(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?")]
+    [GeneratedRegex(@"((http|https)://){0,1}[\w\-_]+(\.[\w\-_]+)+([\w\-.,@?^=%&:/~+#]*[\w\-@?^=%&/~+#])?")]
     private static partial Regex UrlRegex();
-
-    [GeneratedRegex(@"(?<!://)live\.bilibili\.com")]
-    private static partial Regex BLiveRegex();
 }
